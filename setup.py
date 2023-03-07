4f416c32ba515f7312427d5581dfac9b6a42669d
ramkasula2019 <48782812+ramkasula2019@users.noreply.github.com>
2023-03-07 09:10:24 +0545

    set up for 1.0.2

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from distutils.core import setup
 setup(
     name = 'django-remote-control',
     packages = ['remote_control'],
-    version = '1.0.1',
+    version = '1.0.2',
     install_requires=[
         'requests',
     ],
     description = 'Securely call commands in other django applications.',
     author = 'Luke Sapan',
     author_email = 'lukevsapan@gmail.com',
-    url = 'https://github.com/lsapan/django-remote-control',
-    download_url = 'https://github.com/lsapan/django-remote-control/archive/1.0.1.tar.gz',
+    url = 'https://github.com/ramkasula2019/django-remote-control',
+    download_url = 'https://github.com/ramkasula2019/django-remote-control/archive/1.0.2.tar.gz',
     keywords = ['django', 'remote' 'commands', 'rpc'],
     classifiers = [],
 )
